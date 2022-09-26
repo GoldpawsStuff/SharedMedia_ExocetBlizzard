ba13e2cc8e5ffad242bddbbadb14c08e078b629c
Lars Norberg <github@larsnorbergofficial.com>
2022-09-26 13:05:00 +0200

    fix font paths

--- SharedMedia_ExocetBlizzard/Main.lua
+++ SharedMedia_ExocetBlizzard/Main.lua
@@ -4,5 +4,5 @@ local ruRU, western = LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_western
 local flag = ruRU + western
 local FONT = LSM.MediaType.FONT
 
-LSM:Register(FONT, "ExocetBlizzardLight", 		[[Interface\AddOns\SharedMediaExocetBlizzard\Media\ExocetBlizzardLight.ttf]], flag)
-LSM:Register(FONT, "ExocetBlizzardMedium", 		[[Interface\AddOns\SharedMediaExocetBlizzard\Media\ExocetBlizzardMedium.ttf]], flag)
+LSM:Register(FONT, "ExocetBlizzardLight", 		[[Interface\AddOns\SharedMedia_ExocetBlizzard\Media\ExocetBlizzardLight.ttf]], flag)
+LSM:Register(FONT, "ExocetBlizzardMedium", 		[[Interface\AddOns\SharedMedia_ExocetBlizzard\Media\ExocetBlizzardMedium.ttf]], flag)
